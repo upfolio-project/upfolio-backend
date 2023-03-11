ff5a1ebbce2de84cd204fc11368dcdefe204a65e
user <haghee0eip6ohjoh@protonmail.com>
2023-03-11 19:04:24 +0500

    model(user): remove middle name, make class embeddable

--- src/main/java/com/up/upfolio/entities/UserRealNameModel.java
+++ src/main/java/com/up/upfolio/entities/UserRealNameModel.java
@@ -1,18 +1,26 @@
-package com.up.upfolio.model.user;
+package com.up.upfolio.entities;
 
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@Embeddable
 public class UserRealNameModel {
     private static final int MAX_FIELD_LENGTH = 30;
 
+    @NotBlank(message = "First name must be non-empty")
     private String firstName;
-    private String middleName;
+
+    @NotBlank(message = "Last name must be non-empty")
     private String lastName;
 
-    public boolean isValid() {
+    public UserRealNameModel() {
+    }
+
+    public boolean checkValid() {
         if (firstName == null)
             return false;
 
@@ -20,7 +28,7 @@ public class UserRealNameModel {
         if (stripped.isEmpty() || stripped.length() > MAX_FIELD_LENGTH)
             return false;
 
-        return fieldLengthLessOrNull(middleName) && fieldLengthLessOrNull(lastName);
+        return fieldLengthLessOrNull(lastName);
     }
 
     private boolean fieldLengthLessOrNull(String s) {
