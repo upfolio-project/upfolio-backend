19c8ba6e947548ccb658741a2443cc29e8c20644
user <haghee0eip6ohjoh@protonmail.com>
2023-03-21 23:27:22 +0500

    auth(controller): implement login controller

--- src/main/java/com/up/upfolio/repositories/UserRepository.java
+++ src/main/java/com/up/upfolio/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package com.up.upfolio.repositories;
 import com.up.upfolio.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
-
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
