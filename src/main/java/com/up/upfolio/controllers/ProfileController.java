ccf6ccdf63c3e85a36c41ea9dc876c90c1ee167a
user <haghee0eip6ohjoh@protonmail.com>
2023-03-22 23:07:24 +0500

    api(getMe): add temporary mock data

--- src/main/java/com/up/upfolio/controllers/ProfileController.java
+++ src/main/java/com/up/upfolio/controllers/ProfileController.java
@@ -1,23 +1,47 @@
 package com.up.upfolio.controllers;
 
+import com.up.upfolio.entities.UserRealNameModel;
 import com.up.upfolio.model.api.response.profile.GetMeResponse;
 import com.up.upfolio.model.api.response.profile.GetProfileResponse;
-import org.apache.commons.lang3.NotImplementedException;
+import com.up.upfolio.model.user.ProfileModel;
+import com.up.upfolio.model.user.ProfileStatus;
+import com.up.upfolio.model.user.ProfileType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/profile")
 public class ProfileController extends BaseController {
     @GetMapping("/getMe")
     public GetMeResponse getMe() {
-        throw new NotImplementedException();
+        // TODO implement the profile CRUD
+
+        return new GetMeResponse("mock", "https://upfolio.ru/mock");
     }
 
     @GetMapping("/{username}")
     public GetProfileResponse getProfile(@PathVariable String username) {
-        throw new NotImplementedException();
+        // TODO implement the profile CRUD
+
+        ProfileModel model = new ProfileModel();
+
+        model.setStatus(ProfileStatus.LOOKING_FOR_JOB);
+        model.setType(ProfileType.PUBLIC);
+        model.setBio("Test bio");
+        model.setTags(List.of("angular", "react", "nextjs", "frontend"));
+        model.setProfilePhotoUrl("https://upfolio.ru/assets/no-img.png");
+        model.setVerified(true);
+        model.setUsername("mock");
+        model.setRealName(new UserRealNameModel("Ivan", "Mockov"));
+        model.setDateOfBirth("2000-12-24");
+        model.setRegistered(OffsetDateTime.now().minus(Duration.ofDays(10)));
+
+        return new GetProfileResponse(model);
     }
 }
--- src/main/java/com/up/upfolio/model/api/response/profile/GetProfileResponse.java
+++ src/main/java/com/up/upfolio/model/api/response/profile/GetProfileResponse.java
@@ -2,11 +2,13 @@ package com.up.upfolio.model.api.response.profile;
 
 import com.up.upfolio.model.api.response.BaseApiResponse;
 import com.up.upfolio.model.user.ProfileModel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class GetProfileResponse extends BaseApiResponse {
     private ProfileModel profile;
 }
